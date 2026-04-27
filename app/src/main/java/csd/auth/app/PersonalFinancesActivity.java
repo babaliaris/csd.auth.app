fcb2f5983a1e993241255f99ff7363802a4b92d8
Andreas-Galanakis <agalana@csd.auth.gr>
2026-04-27 23:27:19 +0300

    1

---
+++ app/src/main/java/csd/auth/app/PersonalFinancesActivity.java
@@ -0,0 +1,48 @@
+package csd.auth.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+
+
+public class PersonalFinancesActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_personal_finances);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+
+        });
+    }
+    public void OpenAddIncomeMenu(View view) {
+        Intent i = new Intent(this, AddIncomeActivity.class);
+        startActivity(i);
+    }
+
+    public void OpenAddExpenseMenu(View view) {
+        Intent i = new Intent(this, AddExpenseActivity.class);
+        startActivity(i);
+    }
+
+    public void OpenTransactionHistoryMenu(View view) {
+        Intent i = new Intent(this, TransactionHistoryActivity.class);
+        startActivity(i);
+    }
+}
